fa64652939ff9e67eddcd90dd2e80d7dc8e0148c
thebombvass <mevomvas@bu.edu>
2019-08-09 14:36:07 +0200

    added most engaged attendance stats

--- JS/TGIF.js
+++ JS/TGIF.js
@@ -4,34 +4,11 @@
 // *********** FUNCTIONS ******************* //
 
 // function to organzie the senate data from JSON into a pretty table as well as sub in full name for party //
-function senateDataInTable(data) {
+function dataInTable(data) {
     let result = "<thead><th> First Name </th><th> Middle Name </th><th> Last Name </th><th> Party </th><th> State </th><th> Percentage of Votes with Party</th></thead><tbody>"
     let resultnext;
   for (i = 0; i < data.length; i++) {
-      if (data[i].party == "R") {
-        resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Republican </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-      } else if (data[i].party == "D") {
-        resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Democrat </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-      } else if (data[i].party == "I") {
-        resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Independent </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-      }
-    result += resultnext;
-    }
-  return result;
-}
-
-// function that does the same as senateDtaInTable but for house data //
-function houseDataInTable(data) {
-    let result = "<thead><th> First Name </th><th> Middle Name </th><th> Last Name </th><th> Party </th><th> State </th><th> Percentage of Votes with Party</th></thead><tbody>"
-    let resultnext;
-  for (i = 0; i < data.length; i++) {
-    if (data[i].party == "R") {
-      resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Republican </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-    } else if (data[i].party == "D") {
-      resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Democrat </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-    } else if (data[i].party == "I") {
-      resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> Independent </td><td> ${data[i].state} </td><td id=pctPartyCol> ${data[i].votes_with_party_pct} %</td></tr>`;
-    }
+    resultnext = `<tr><td><a href="${data[i].url}"> ${data[i].first_name}</a></td><td> ${data[i].middle_name || ""} </td><td> ${data[i].last_name} </td><td> ${(data[i].party == "R" ? "Republican":data[i].party == "D" ?"Democrat":"Independent")} </td><td> ${data[i].state} </td><td class=centeredCol> ${data[i].votes_with_party_pct} %</td></tr>`;
     result += resultnext;
     }
   return result;
@@ -39,12 +16,4 @@ function houseDataInTable(data) {
 
 // ********************* CALLING FUNCTIONS ****************** //
 
-//filling the table element with the organized table from senateDataInTable function// 
-if (document.getElementById("senateData")) {
-    document.getElementById("senateData").innerHTML = senateDataInTable (senateData.results[0].members);
-}
-
-//filling the table element with the organized table from houseDataInTable function// 
-if (document.getElementById("houseData")) {
-    document.getElementById("houseData").innerHTML = senateDataInTable (houseData.results[0].members);
-}
+document.getElementById('table').innerHTML = dataInTable (data.results[0].members)
--- JS/senate-stats-calculations.js
+++ JS/senate-stats-calculations.js
@@ -1,79 +1,141 @@
-//statistics for senate// 
+//statistics for senate//
 
 //creating statistics object for 113th Senate //
-senateStats = {numberOfDems: 0, numberOfReps: 0, numberOfInds: 0, 
-    demsVoteOnParty: 0, repsVoteOnParty: 0, indsVoteOnParty: 0, membersVoteAgainstParty: 0,
-    membersVoteWithParty: 0, membersMissedVotesMost: 0, 
-    membersMissedVotesLeast: 0 }
+senateStats = {
+  numberOfDems: 0,
+  numberOfReps: 0,
+  numberOfInds: 0,
+  demsVoteOnParty: 0,
+  repsVoteOnParty: 0,
+  indsVoteOnParty: 0,
+  membersVoteAgainstParty: 0,
+  membersVoteWithParty: 0,
+  membersMostEngaged: 0,
+  membersLeastEngaged: 0
+};
+
+
 
 //*****************FUNCTIONS ******************//
 
 //function to get number of members in each party//
-function numberOfMembersCalc (array) {
-    for (i = 0; i < array.length; i++) {
-        if (array[i].party == "R") {
-            senateStats.numberOfReps += 1;
-          } else if (array[i].party == "D") {
-            senateStats.numberOfDems +=1
-          } else if (array[i].party == "I") {
-            senateStats.numberOfInds += 1
-          }
+function numberOfMembersCalc(array) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i].party == "R") {
+      senateStats.numberOfReps += 1;
+    } else if (array[i].party == "D") {
+      senateStats.numberOfDems += 1;
+    } else if (array[i].party == "I") {
+      senateStats.numberOfInds += 1;
     }
+  }
 }
 
-//function to get average Democrat vote with party and average Republican vote with party 
-function votesWithPartyCalc (array) {
-    //get the list of percent of votes with party for dems and reps//
-    let repPcts = []
-    let demPcts = []
-    let indPcts = []
-    for (i = 0; i < array.length; i++) {
-        if (array[i].party == "R") {
-            repPcts.push(array[i].votes_with_party_pct)
-          } else if (array[i].party == "D") {
-            demPcts.push(array[i].votes_with_party_pct)
-          } else {
-            indPcts.push(array[i].votes_with_party_pct)
-          }
+//function to get average Democrat vote with party and average Republican vote with party
+function votesWithPartyCalc(array) {
+  //get the list of percent of votes with party for dems and reps//
+  let repPcts = [];
+  let demPcts = [];
+  let indPcts = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i].party == "R") {
+      repPcts.push(array[i].votes_with_party_pct);
+    } else if (array[i].party == "D") {
+      demPcts.push(array[i].votes_with_party_pct);
+    } else {
+      indPcts.push(array[i].votes_with_party_pct);
     }
-    //turn the array into an average, reps first then dems//
-    const add = (a,b) => a+b
-    senateStats.repsVoteOnParty = ((repPcts.reduce(add))/repPcts.length).toFixed(2)
-    senateStats.demsVoteOnParty = ((demPcts.reduce(add))/demPcts.length).toFixed(2)
-    senateStats.indsVoteOnParty = ((indPcts.reduce(add))/indPcts.length).toFixed(2)
+  }
+  //turn the array into an average, reps first then dems//
+  const add = (a, b) => a + b;
+  senateStats.repsVoteOnParty = (repPcts.reduce(add) / repPcts.length).toFixed(
+    2
+  );
+  senateStats.demsVoteOnParty = (demPcts.reduce(add) / demPcts.length).toFixed(
+    2
+  );
+  senateStats.indsVoteOnParty = (indPcts.reduce(add) / indPcts.length).toFixed(
+    2
+  );
 }
 
-function heyonesecjustsettingup (array) {
-    //get the list of percent of votes with party for dems and reps//
-    let repPcts = []
-    let demPcts = []
-    let indPcts = []
-    for (i = 0; i < array.length; i++) {
-        if (array[i].party == "R") {
-            repPcts.push(array[i].votes_with_party_pct)
-          } else if (array[i].party == "D") {
-            demPcts.push(array[i].votes_with_party_pct)
-          } else {
-            indPcts.push(array[i].votes_with_party_pct)
-          }
+
+//get a sorted list of senators by missed votes//
+function sortByEngaged(data) {
+  sortedEngagementArray = [];
+  for (i = 0; i < data.length; i++) {
+      sortedEngagementArray.push({
+        name: data[i].first_name,
+        lname: data[i].last_name,
+        missed_votes: data[i].missed_votes,
+        missed_votes_pct: data[i].missed_votes_pct
+      })
     }
-    //turn the array into an average, reps first then dems//
-    const add = (a,b) => a+b
-    senateStats.repsVoteOnParty = ((repPcts.reduce(add))/repPcts.length).toFixed(2)
-    senateStats.demsVoteOnParty = ((demPcts.reduce(add))/demPcts.length).toFixed(2)
-    senateStats.indsVoteOnParty = ((indPcts.reduce(add))/indPcts.length).toFixed(2)
+  sortedEngagementArray.sort(function(a,b) {
+    return a.missed_votes_pct - b.missed_votes_pct;
+  });
+  return sortedEngagementArray;
 }
+
+//calculates the membersMostEngaged statistic using sorted list in sortByEngaged//
+function mostEngaged(sortedArray, arrayToSum) {
+  totalMembers = arrayToSum.reduce((a,b)=> a+b,0);
+  tenPercentAmt = Math.ceil(totalMembers * 0.1);
+  topten = [];
+  for (s = 0; s < tenPercentAmt; s++) {
+    topten.push(sortedArray[s]);
+  }
+  //makese sure that anyone tied with the lowest number of votes is included//
+  while (true) {
+    if (
+      sortedArray[topten.length - 1].missed_votes_pct===
+      sortedArray[topten.length].missed_votes_pct
+    ) {
+      topten.push(sortedArray[topten.length]);
+    } else {
+      break;
+    }
+  }
+  senateStats.membersMostEngaged = topten
+}
+
+//creating the table of 'Most Engaged (Top 10% Attendance)' using membersMostEngaged stat//
+function createMostEngagedTable (array){
+  let resultnext=""
+  for (i = 0; i < array.length; i++) {
+    result = `<tr><td>${array[i].name} ${array[i].lname} </td><td class=centeredCol> ${array[i].missed_votes} </td><td class=centeredCol> ${array[i].missed_votes_pct} %</td></tr>`;
+    resultnext += result;
+    }
+  return resultnext;
+}
+
 
 //****************CALLING FUNCTIONS ****************//
 
 // functions to fill senateStats with the right numbers//
-numberOfMembersCalc (senateData.results[0].members)
-votesWithPartyCalc(senateData.results[0].members)
+numberOfMembersCalc(data.results[0].members);
+votesWithPartyCalc(data.results[0].members);
 
 //stuff to fill 'senate at a glance' table//
-document.getElementById("repNum").innerHTML = senateStats.numberOfReps
-document.getElementById("repPct").innerHTML = ` ${senateStats.repsVoteOnParty} %`
-document.getElementById("demNum").innerHTML = senateStats.numberOfDems
-document.getElementById("demPct").innerHTML = ` ${senateStats.demsVoteOnParty} %`
-document.getElementById("indNum").innerHTML = senateStats.numberOfInds
-document.getElementById("indPct").innerHTML = ` ${senateStats.indsVoteOnParty} %`
+document.getElementById("repNum").innerHTML = senateStats.numberOfReps;
+document.getElementById("repPct").innerHTML = ` ${
+  senateStats.repsVoteOnParty
+} %`;
+document.getElementById("demNum").innerHTML = senateStats.numberOfDems;
+document.getElementById("demPct").innerHTML = ` ${
+  senateStats.demsVoteOnParty
+} %`;
+document.getElementById("indNum").innerHTML = senateStats.numberOfInds;
+document.getElementById("indPct").innerHTML = ` ${
+  senateStats.indsVoteOnParty
+} %`;
+
+//getting the top 10%//
+mostEngaged(sortByEngaged(data.results[0].members), [
+  senateStats.numberOfDems,
+  senateStats.numberOfReps,
+  senateStats.numberOfInds
+]);
+
+//stuff to fill 'Most Engaged (Top 10% Attendance)' table//
+document.getElementById("mostEngaged").innerHTML = createMostEngagedTable(senateStats.membersMostEngaged);
